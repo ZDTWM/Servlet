c2bcf4d5fe729b5ce5ca6ecad3a644ad56ae58be
TWM <480086311@qq.com>
2018-03-16 17:48:31 +0800

    返回响应

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -1,12 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet{
-	protected void doPost(HttpServletRequest request,HttpServletResponse response){
+	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException{
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
 		System.out.println("name:" + name);
 		System.out.println("password:" + password);
+		
+		String html = null;
+		if("admin".equals(name) && "123".equals(password))
+			html = "<div style='color:green'>success</div>";
+		else
+			html = "<div style='color:red'>success</div>";
+		
+		PrintWriter pw = response.getWriter();
+		pw.println(html);
 	}
 }

f94f2db25b06b3250ec0867c754b8ceeba8b9679
TWM <480086311@qq.com>
2018-03-18 12:58:37 +0800

    请求以及响应设置中文

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet{
 	protected void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		//请求设置中文
 		request.setCharacterEncoding("UTF-8");
 		
 		String name = request.getParameter("name");
@@ -19,6 +20,7 @@ public class LoginServlet extends HttpServlet{
 			html = "<div style='color:green'>登陆成功</div>";
 		else
 			html = "<div style='color:red'>登陆失败</div>";
+		//服务器响应设置中文
 		response.setContentType("text/html; charset=UTF-8");
 		
 		PrintWriter pw = response.getWriter();

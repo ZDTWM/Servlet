f84f523f594669842b0bf2a994d3f4a266d25a16
TWM <480086311@qq.com>
2018-03-19 11:50:35 +0800

    设置响应编码

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Date;
 import java.util.Enumeration;
 
@@ -9,18 +10,15 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 	
-	public void doGet(HttpServletRequest request,HttpServletResponse response){
-		
-		Enumeration<String> headerNames = request.getHeaderNames();
-		while(headerNames.hasMoreElements()){
-			String header = headerNames.nextElement();
-			String value = request.getHeader(header);
-			System.out.printf("%s\t%s%n",header,value);
-		}
-		
+	public void doGet(HttpServletRequest request,HttpServletResponse response){		
 		try {
-			response.getWriter().println("<h1>Hello Servlet!</h1>");
-			response.getWriter().println(new Date().toString());
+            response.setContentType("text/html; charset=UTF-8");
+//          response.setCharacterEncoding("UTF-8");
+			
+            PrintWriter pw = response.getWriter();
+            pw.println("<h1>Hello Servlet</h1>");
+ 
+            
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -28,8 +26,5 @@ public class HelloServlet extends HttpServlet {
 		
 	}
 	
-	public void init(ServletConfig config){
-		System.out.println("init of Hello Servlet");
-	}
 	
 }

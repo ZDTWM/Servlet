bb88ca02a13f68a5175c3381d3870205f724fd51
TWM <480086311@qq.com>
2018-03-19 11:08:14 +0800

    获取头信息

--- src/HelloServlet.java
+++ src/HelloServlet.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.util.Date;
+import java.util.Enumeration;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +10,14 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest request,HttpServletResponse response){
+		
+		Enumeration<String> headerNames = request.getHeaderNames();
+		while(headerNames.hasMoreElements()){
+			String header = headerNames.nextElement();
+			String value = request.getHeader(header);
+			System.out.printf("%s\t%s%n",header,value);
+		}
+		
 		try {
 			response.getWriter().println("<h1>Hello Servlet!</h1>");
 			response.getWriter().println(new Date().toString());

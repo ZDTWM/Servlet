acb9e9677d4e8ebe98eeed8d7aba1fdd5e37e32f
TWM <480086311@qq.com>
2018-03-19 09:05:38 +0800

    request的常见方法

--- src/LoginServlet.java
+++ src/LoginServlet.java
@@ -16,12 +16,42 @@ public class LoginServlet extends HttpServlet{
 		System.out.println("name:" + name);
 		System.out.println("password:" + password);
 		
+		//浏览器发出请求时的完整URL，包括协议 主机名 端口(如果有)
+		System.out.println("浏览器发出请求时的完整URL，包括协议 主机名 端口(如果有): " + request.getRequestURL());
 		
+		//浏览器发出请求的资源名部分，去掉了协议和主机名:
+		System.out.println("浏览器发出请求的资源名部分，去掉了协议和主机名: " + request.getRequestURI());
+		
+		//请求行中的参数部分:
+		System.out.println("请求行中的参数部分: " + request.getQueryString());
+		
+		//浏览器所处于的客户机的IP地址: 
+		System.out.println("浏览器所处于的客户机的IP地址: " + request.getRemoteAddr());
+		
+		//浏览器所处于的客户机的主机名:
+		System.out.println("浏览器所处于的客户机的主机名: " + request.getRemoteHost());
+		
+		//浏览器所处于的客户机使用的网络端口: 
+		System.out.println("浏览器所处于的客户机使用的网络端口: " + request.getRemotePort());
+		
+		//服务器的IP地址: 
+		System.out.println("服务器的IP地址: " + request.getLocalAddr());
+		
+		//服务器的主机名: 
+		System.out.println("服务器的主机名: " + request.getLocalName());
+		
+		//得到客户机请求方式:
+		System.out.println("得到客户机请求方式: " + request.getMethod());
+		
+		String html = "null";
 		if("admin".equals(name) && "123".equals(password))
-			//服务端跳转
-			request.getRequestDispatcher("success.html").forward(request, response);
+			 html = "<div style='color:green'>登录成功</div>";
 		else
-			//客户端跳转
-			response.sendRedirect("fail.html");
+			 html = "<div style='color:red'>登录失败</div>";
+		
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.println(html);
+		
 	}
 }
